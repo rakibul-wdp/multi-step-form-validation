6013505f365c28dbf036118a84d280b612ccc8be
rakibul-wdp <rakibul.wdp@gmail.com>
2024-09-14 12:29:17 +0600

    removed un use style code

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App.tsx";
-import "./index.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
